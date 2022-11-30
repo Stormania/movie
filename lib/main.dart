8a6f34cc11ea5f53a3e2e121ba6f2714ff74f1ae
Ştefan <Stormania94@gmail.com>
2022-11-30 18:10:48 +0200

    modified:   lib/main.dart 	renamed:    movie.png -> movies.png 	modified:   pubspec.lock 	modified:   pubspec.yaml 	modified:   test/widget_test.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,142 @@
+//import 'package:flutter/cupertino.dart';
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
 void main() {
-  runApp(const Movie());
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MoviesPage(
+        title: 'Movies',
+      ),
+    ),
+  );
 }
 
-class Movie extends StatelessWidget {
-  const Movie({super.key});
-
+class Movies extends StatelessWidget {
+  const Movies({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return const DecoratedBox(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          // ignore: always_specify_types
+          colors: [
+            Colors.blue,
+            Colors.purple,
+          ],
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class MoviesPage extends StatefulWidget {
+  const MoviesPage({super.key, required this.title});
   final String title;
-
   @override
-  State<MyHomePage> createState() => _MoviePageState();
+  State<MoviesPage> createState() => _MoviePageState();
 }
 
-class _MoviePageState extends State<MyHomePage> {
+class _MoviePageState extends State<MoviesPage> {
+  bool _isLoading = true;
+  final List<String> _titleMovie = <String>[];
+  final List<String> _imageMovie = <String>[];
+  final List<int> _yearMovie = <int>[];
+  final List<int> _runtimeMovie = <int>[];
+
+  @override
+  void initState() {
+    super.initState();
+    _getMovies();
+  }
+
+  void _getMovies() {
+    get(Uri.parse('https://yts.mx/api/v2/list_movies.json')) //
+        .then((Response response) {
+      response.body;
+
+      final Map<String, dynamic> map = jsonDecode(response.body) as Map<String, dynamic>;
+      final Map<String, dynamic> data = map['data'] as Map<String, dynamic>;
+      final List<dynamic> movies = data['movies'] as List<dynamic>;
+
+      final Iterable<String> titleMovie = movies //
+          .cast<Map<dynamic, dynamic>>()
+          .map((Map<dynamic, dynamic> item) => item['title'] as String);
+
+      final Iterable<String> imageMovie = movies //
+          .cast<Map<dynamic, dynamic>>()
+          .map((Map<dynamic, dynamic> item) => item['medium_cover_image'] as String);
+
+      final Iterable<int> yearMovie = movies //
+          .cast<Map<dynamic, dynamic>>()
+          .map((Map<dynamic, dynamic> item) => item['year'] as int);
+
+      final Iterable<int> runtimeMovie = movies //
+          .cast<Map<dynamic, dynamic>>()
+          .map((Map<dynamic, dynamic> item) => item['runtime'] as int);
+
+      setState(() {
+        _isLoading = false;
+        _titleMovie.addAll(titleMovie);
+        _imageMovie.addAll(imageMovie);
+        _yearMovie.addAll(yearMovie);
+        _runtimeMovie.addAll(runtimeMovie);
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Column(),
+      body: Builder(
+        builder: (BuildContext context) {
+          if (_isLoading) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return PageView.builder(
+            itemBuilder: (BuildContext context, int index) {
+              return Column(
+                children: <Widget>[
+                  const SizedBox(height: 150),
+                  AnimatedContainer(
+                    duration: const Duration(
+                      seconds: 1,
+                    ),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    child: Image.network(_imageMovie[index]),
+                  ),
+                  const SizedBox(height: 30),
+                  Column(
+                    children: <Widget>[
+                      Text(
+                        _titleMovie[index],
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(fontSize: 30),
+                      ),
+                      const SizedBox(height: 10),
+                      Text(
+                        'Release Year: ${_yearMovie[index]}',
+                        style: const TextStyle(fontSize: 18, color: Colors.grey),
+                      ),
+                      Text(
+                        'Duration: ${_runtimeMovie[index]} minutes',
+                        style: const TextStyle(fontSize: 18, color: Colors.grey),
+                      ),
+                    ],
+                  )
+                ],
+              );
+            },
+          );
+        },
       ),
     );
   }
